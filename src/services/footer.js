868c8d77e93dec647f4cfbd02de1521122156534
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-06-06 11:23:05 +0300

    add footer constants

---
+++ src/services/footer.js
@@ -0,0 +1,60 @@
+import facebook from '../images/icons/social-media/facebook.png';
+import twitter from '../images/icons/social-media/twitter.png';
+import instagram from '../images/icons/social-media/instagram.png';
+import linkedin from '../images/icons/social-media/linkedin.png';
+
+const SITEMAP = [
+  {
+    label: 'About us',
+    url: '#0',
+  },
+  {
+    label: 'Contact us',
+    url: '#0',
+  },
+  {
+    label: 'Track Order',
+    url: '#0',
+  },
+  {
+    label: 'Terms & Conditions',
+    url: '#0',
+  },
+  {
+    label: 'Privacy Policy',
+    url: '#0',
+  },
+  {
+    label: 'Sell With Us',
+    url: '#0',
+  },
+  {
+    label: 'Shipping And Returns',
+    url: '#0',
+  },
+];
+
+const SOCIAL_MEDIA = [
+  {
+    label: '/YESHTERY',
+    img: facebook,
+    url: '#0',
+  },
+  {
+    label: '/YESHTERY',
+    img: linkedin,
+    url: '#0',
+  },
+  {
+    label: '/YESHTERY',
+    img: instagram,
+    url: '#0',
+  },
+  {
+    label: '/YESHTERY',
+    img: twitter,
+    url: '#0',
+  },
+];
+
+export { SOCIAL_MEDIA, SITEMAP };

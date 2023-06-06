577fc356d90cc40647f293a200fa27643941adf7
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-06-06 11:24:01 +0300

    create navbar constant

---
+++ src/services/navbar.js
@@ -0,0 +1,33 @@
+const CATEGORY = [
+  {
+    label: 'Men',
+    url: '#0',
+  },
+  {
+    label: 'Women',
+    url: '#0',
+  },
+  {
+    label: 'Unisex',
+    url: '#0',
+  },
+  {
+    label: 'Kids',
+    url: '#0',
+  },
+  {
+    label: 'Best Seller',
+    url: '#0',
+  },
+  {
+    label: 'New Arrivals',
+    url: '#0',
+  },
+  {
+    label: 'Offers',
+    url: '#0',
+    isOffer: true,
+  },
+];
+
+export { CATEGORY };

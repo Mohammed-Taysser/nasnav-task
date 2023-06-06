abd66733cc9e707c4d65ff5a3d3bfd3a1bfc6d0f
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-06-06 11:29:59 +0300

    add mising border style

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 
 function Banner(props) {
   return (
-    <div className='container'>
-      <nav aria-label='breadcrumb' className='banner'>
-        <ol className='breadcrumb'>
-          {props.urls.map((link, index) => (
-            <li key={index} className='breadcrumb-item'>
-              <a href={link.url}>{link.label}</a>
+    <div className='banner'>
+      <div className='container'>
+        <nav aria-label='breadcrumb'>
+          <ol className='breadcrumb'>
+            {props.urls.map((link, index) => (
+              <li key={index} className='breadcrumb-item'>
+                <a href={link.url}>{link.label}</a>
+              </li>
+            ))}
+            <li className='breadcrumb-item active' aria-current='page'>
+              {props.title}
             </li>
-          ))}
-          <li className='breadcrumb-item active' aria-current='page'>
-            {props.title}
-          </li>
-        </ol>
-      </nav>
+          </ol>
+        </nav>
+      </div>
     </div>
   );
 }

85ab6e5313706224ad7410ff74e94e45cd66088a
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-06-07 12:20:29 +0300

    add condifion if cart is empty

--- src/components/CartOffcanvas.jsx
+++ src/components/CartOffcanvas.jsx
@@ -26,48 +26,57 @@ function CartOffcanvas() {
       <div className='offcanvas-body'>
         <h3 className='title'>My Cart</h3>
         <h5 className='font-bold mt-5'>Cart Summary </h5>
-        <div className='cart-wrapper my-4'>
-          {cart.map((item) => (
-            <div key={item.id} className='single-cart'>
-              <div className='row'>
-                <div className='col-4'>
-                  <div className='image'>
-                    <img
-                      src={item.image}
-                      alt={item.title}
-                      width={104}
-                      height={104}
-                    />
-                  </div>
-                </div>
-                <div className='col-8'>
-                  <div className='info'>
-                    <h6 className='font-bold text-sm mb-2 product-title'>
-                      {item.title}
-                    </h6>
-                    <div className='font-bold text-xs mb-2'>
-                      Quantity: {item.quantity}
-                    </div>
-                    <div className='flex justify-between items-center'>
-                      <div className='price'>
-                        {item.price.toLocaleString()}{' '}
-                        <small className='text-xs'>{item.currency}</small>{' '}
+        {cart.length > 0 ? (
+          <>
+            {' '}
+            <div className='cart-wrapper my-4'>
+              {cart.map((item) => (
+                <div key={item.id} className='single-cart'>
+                  <div className='row'>
+                    <div className='col-4'>
+                      <div className='image'>
+                        <img
+                          src={item.image}
+                          alt={item.title}
+                          width={104}
+                          height={104}
+                        />
                       </div>
-                      <button className='yt-btn sm'>Remove</button>
+                    </div>
+                    <div className='col-8'>
+                      <div className='info'>
+                        <h6 className='font-bold text-sm mb-2 product-title'>
+                          {item.title}
+                        </h6>
+                        <div className='font-bold text-xs mb-2'>
+                          Quantity: {item.quantity}
+                        </div>
+                        <div className='flex justify-between items-center'>
+                          <div className='price'>
+                            {item.price.toLocaleString()}{' '}
+                            <small className='text-xs'>{item.currency}</small>{' '}
+                          </div>
+                          <button className='yt-btn sm'>Remove</button>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <h5 className='title text-dark mt-4'>
-          Total: {total.toLocaleString()} LE
-        </h5>
-        <div className='flex gap-3 items-center w-100 mt-8'>
-          <button className='w-50 yt-btn secondary'>Review Cart</button>
-          <button className='w-50 yt-btn'>Complete Checkout</button>
-        </div>
+            <h5 className='title text-dark mt-4'>
+              Total: {total.toLocaleString()} LE
+            </h5>
+            <div className='flex gap-3 items-center w-100 mt-8'>
+              <button className='w-50 yt-btn secondary'>Review Cart</button>
+              <button className='w-50 yt-btn'>Complete Checkout</button>
+            </div>
+          </>
+        ) : (
+          <h6 className='font-bold mt-5 text-center'>
+            You have no items yet..
+          </h6>
+        )}
       </div>
     </div>
   );
